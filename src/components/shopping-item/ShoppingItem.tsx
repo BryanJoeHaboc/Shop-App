87eed4e629f4825fd25c4b00531c6869be01aaab
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-04-22 12:42:19 +0800

    feat: added ShoppingItem component

---
+++ src/components/shopping-item/ShoppingItem.tsx
@@ -0,0 +1,54 @@
+import { useAppDispatch } from "../../app/hooks";
+import ShoppingItem from "../../../interfaces/shoppingItem";
+import {
+  addItem,
+  subtractItem,
+} from "../../features/shoppingCart/shoppingCartSlice";
+
+const ShoppingItemComponent = ({ product, quantity, _id }: ShoppingItem) => {
+  const dispath = useAppDispatch();
+
+  const shoppingItem = {
+    _id,
+    product,
+    quantity: 1,
+  };
+
+  const handleIncreaseItemQty = (shoppingItem: ShoppingItem) => {
+    dispath(addItem(shoppingItem));
+  };
+
+  const handleDecreaseItemQty = (shoppingItem: ShoppingItem) => {
+    dispath(subtractItem(shoppingItem));
+  };
+
+  return (
+    <div className="shopping_cart_items">
+      <img src={product.imageUrl} alt="" />
+      <div className="shopping_cart_items_info">
+        <p className="bold_900">{product.name}</p>
+        <p className="bold_600">Price: {product.price}</p>
+        <p className="bold_600">
+          {" "}
+          <span
+            onClick={() => {
+              handleDecreaseItemQty(shoppingItem);
+            }}
+          >
+            Qty:
+          </span>{" "}
+          &#60; {quantity}{" "}
+          <span
+            onClick={() => {
+              handleIncreaseItemQty(shoppingItem);
+            }}
+          >
+            &#62;
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ShoppingItemComponent;
--- src/pages/ShoppingCart/ShoppingCart.tsx
+++ src/pages/ShoppingCart/ShoppingCart.tsx
@@ -1,54 +1,24 @@
 import "./ShoppingCart.scss";
 
-import { useAppSelector, useAppDispatch } from "../../app/hooks";
-import ShoppingItem from "../../../interfaces/shoppingItem";
+import { useAppSelector } from "../../app/hooks";
 import { RootState } from "../../app/store";
-import { addItem } from "../../features/shoppingCart/shoppingCartSlice";
-import { useState } from "react";
+import ShoppingItem from "../../../interfaces/shoppingItem";
+import ShoppingItemComponent from "../../components/shopping-item/ShoppingItem";
 
 const ShoppingCartComponent = () => {
   const shoppingCart = useAppSelector((state: RootState) => state.shoppingCart);
-  const [cartItemId, setCartItemId] = useState("");
-
-  const dispath = useAppDispatch();
-  const handleIncreaseItemQty = (item) => {
-    dispath(addItem(item));
-  };
-
-  const handleDecreaseItemQty = () => {};
 
   return (
     <div className="shopping_cart_container">
       {shoppingCart.items.length ? (
-        shoppingCart.items.map(({ product, quantity }: ShoppingItem) => {
-          return (
-            <div className="shopping_cart_items">
-              <img src={product.imageUrl} alt="" />
-              <div className="shopping_cart_items_info">
-                <p className="bold_900">{product.name}</p>
-                <p className="bold_600">Price: {product.price}</p>
-                <p className="bold_600">
-                  {" "}
-                  <span
-                    onClick={() => {
-                      handleDecreaseItemQty(product);
-                    }}
-                  >
-                    Qty:
-                  </span>{" "}
-                  &#60; {quantity}{" "}
-                  <span
-                    onClick={() => {
-                      handleIncreaseItemQty(product);
-                    }}
-                  >
-                    &#62;
-                  </span>
-                </p>
-              </div>
-            </div>
-          );
-        })
+        shoppingCart.items.map(({ product, quantity, _id }: ShoppingItem) => (
+          <ShoppingItemComponent
+            key={_id}
+            _id={_id}
+            product={product}
+            quantity={quantity}
+          />
+        ))
       ) : (
         <h1>No Items!</h1>
       )}
