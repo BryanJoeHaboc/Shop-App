432fa0128955d5d918c893c0aa4343d7f9c0b58e
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-04-23 23:59:45 +0800

    chore: added styles in login

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,4 +1,7 @@
 import { useState, Dispatch } from "react";
+import TextField from "@mui/material/TextField";
+
+import "./LoginPage.scss";
 
 export default function LoginPage() {
   const [firstName, setFirstName] = useState<string>("");
@@ -10,49 +13,72 @@ export default function LoginPage() {
   // for update: event: React.ChangeEvent for submit: event: React.FormEvent for click: event: React.MouseEvent
 
   const handleSubmit = () => {};
-  const handleChange = (
-    setter: Dispatch<string>,
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setter(event.target.value);
+  const handleChange = (setter: Dispatch<string>, event: {}) => {
+    const e = event as React.ChangeEvent<HTMLInputElement>;
+    setter(e.target.value);
   };
 
   return (
     <div className="login_page_container">
-      <form onSubmit={handleSubmit}>
-        <label>
-          First Name:
-          <input
+      <form className="login_page_form_container" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <TextField
             type="text"
             value={firstName}
             onChange={(event) => handleChange(setFirstName, event)}
+            id="standard-basic"
+            label="Standard"
+            variant="standard"
           />
-          Last Name:
-          <input
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <TextField
             type="text"
             value={lastName}
             onChange={(event) => handleChange(setLastName, event)}
+            id="standard-basic"
+            label="Standard"
+            variant="standard"
           />
-          Email:
-          <input
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>{" "}
+          <TextField
             type="text"
             value={email}
             onChange={(event) => handleChange(setEmail, event)}
+            id="standard-basic"
+            label="Standard"
+            variant="standard"
           />
-          Password:
-          <input
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>{" "}
+          <TextField
             type="password"
             value={password}
             onChange={(event) => handleChange(setPassword, event)}
+            id="standard-basic"
+            label="Standard"
+            variant="standard"
           />
-          Confirm Password:
-          <input
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>{" "}
+          <TextField
             type="password"
             value={confirmPassword}
             onChange={(event) => handleChange(setConfirmPassword, event)}
+            id="standard-basic"
+            label="Standard"
+            variant="standard"
           />
-        </label>
-        <input type="submit" value="Submit" />
+        </div>
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
       </form>
     </div>
   );
