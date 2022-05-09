d1f9f72d4a1ebe6ed108b250b9a00db1e8e25c67
Haboc, Bryan Joe <bryanjoehaboc241@gmail.com>
2022-05-10 00:14:32 +0800

    feat: initial design for products under orders page

--- src/pages/orders/Orders.tsx
+++ src/pages/orders/Orders.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
+import FileSaver from "file-saver";
 import { useEffect, useState } from "react";
+
 import { Orders as OrdersInterface } from "../../../interfaces/order";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import ButtonWithTheme from "../../components/custom-button/ButtonWithTheme";
 import { getOrdersFromDB } from "../../features/order/orderSlice";
 import { getUser } from "../../features/user/userSlice";
-import FileSaver from "file-saver";
+import "./Orders.scss";
 
 const Orders = () => {
   const [orders, setOrders] = useState<OrdersInterface[]>([]);
@@ -46,12 +48,33 @@ const Orders = () => {
       {orders.length > 0
         ? orders.map((order) => {
             return (
-              <div key={order._id}>
-                <h1> Order: {order._id}</h1>
-                <ButtonWithTheme
-                  display="Get Invoice"
-                  clickFunc={() => handleGetInvoice(order._id)}
-                />
+              <div className="order__invoices" key={order._id}>
+                <div className="order__invoices__body">
+                  <div className="order__invoices__body__actions">
+                    <h1> Order: {order._id}</h1>
+                    <div className="order__invoices__body__actions__button">
+                      <ButtonWithTheme
+                        display="Get Invoice"
+                        clickFunc={() => handleGetInvoice(order._id)}
+                      />
+                    </div>
+                  </div>
+                  <details>
+                    <summary>Expand to see products</summary>
+                    <div className="order__invoices__body__products">
+                      {order.products.map((product) => {
+                        return (
+                          <div>
+                            <img src={product.imageUrl} alt="product" />
+
+                            <h2>{product.name}</h2>
+                            <h2>${product.price}</h2>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </details>
+                </div>{" "}
               </div>
             );
           })
